3e1e0984a64a773f1998828b8cd5b3b5551deeb2
khaine-zay <131683825+khaine-zay@users.noreply.github.com>
2023-07-18 18:18:43 +0630

    Update utils.py

--- utils.py
+++ utils.py
@@ -40,21 +40,21 @@ def up_progress(current, total, msg: Message):
 class Msg:
 
     def start(msg: Message) -> str:
-        """ return start-message text """
-        txt = f"[‏](https://github.com/M100achuz2/archive-bot.git)היי {msg.from_user.mention}!\n" \
-              "באמצעות בוט זה תוכלו לדחוס קבצים לארכיון. שלחו /zip, ופעלו על פי ההוראות." \
-              "\n\nרובוט זה נוצר על ידי [Yeuda-By](t.me/m100achuzBots) מצוות [רובוטריק](t.me/robottrick)." \
-              "\nלקוד המקור [לחצו כאן](https://github.com/M100achuz2/archive-bot)."
+        """return start-message text"""
+        txt = f"[‏](https://github.com/M100achuz2/archive-bot.git)Hey {msg.from_user.mention}!\n" \
+              "With this bot, you can compress files into an archive. Send /zip to begin, and follow the instructions." \
+              "\n\nThis robot was created by [Yeuda-By](t.me/m100achuzBots) from the team [Robotrick](t.me/robottrick)." \
+              "\nFor the source code, [click here](https://github.com/M100achuz2/archive-bot)."
         return txt
 
-    zip = "שלחו את הקבצים שהנכם רוצים לדחוס, ובסיום שלחו /stopzip לאחר שכל הקבצים ירדו. \n`הבוט תומך בקבצים עד 20mb, " \
-          "ועד 20 קבצים לארכיון אחד.` "
-    too_big = "הקובץ גדול מידי ):"
-    too_much = "ניתן לדחוס עד 20 קבצים בלבד."
-    send_zip = "השתמשו בפקודת /zip בשביל לדחוס קבצים (:"
-    zipping = "מתחיל בדחיסת {} קבצים..."
-    uploading = "מעלה ארכיון..."
-    unknow_error = "התרחשה שגיאה לא ידועה. \nשים לב לסדר הפעולות, ניתן להתחיל מחדש על ידי שליחת /start. \nבדוק אם " \
-                   "שלחת קבצים לדחיסה, ואם חיכית שכולם ירדו. \nבבקשה שלח זה למפתח:\n ```{}``` "
-    downloading = "מוריד קובץ:"
-    zero_files = "לא נשלחו קבצים."
+    zip = "Send the files you want to compress, and when you're done, send /stopzip after all files have been uploaded. \n`The bot supports files up to 20MB each, " \
+          "and up to 20 files per archive.`"
+    too_big = "The file is too large."
+    too_much = "You can only compress up to 20 files."
+    send_zip = "Use the /zip command to compress files (:"
+    zipping = "Starting to compress {} files..."
+    uploading = "Uploading the archive..."
+    unknow_error = "An unknown error occurred. \nPlease check the sequence of actions, you can start over by sending /start. \nMake sure " \
+                   "you sent files for compression and waited for all of them to be uploaded. \nPlease send this to the developer:\n ```{}```"
+    downloading = "Downloading file:"
+    zero_files = "No files were sent."
