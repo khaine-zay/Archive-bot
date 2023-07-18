37dbbff956a79dbb44905e75d7cc6ad59d5782b1
khaine-zay <131683825+khaine-zay@users.noreply.github.com>
2023-07-18 23:33:20 +0630

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ db = Database()
 
 
 class User(db.Entity):
-    uid = PrimaryKey(str, auto=True)
+    uid = PrimaryKey(int, auto=True)
     status = Required(int)  # status-user: "INSERT"/"NOT-INSERT"
 
 
